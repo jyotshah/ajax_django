76831fce8ac027157978a538e868c66e36d52a59
Jyot Chirag Shah <Jshah1717@conestogac.on.ca>
2025-03-21 12:54:04 -0400

    update and delete buttons, and updating post... also added favicon

--- posts/views.py
+++ posts/views.py
@@ -28,6 +28,17 @@ def post_list_and_create(request):
                 
       return render(request,'posts/main.html', context)
 
+def post_detail(request, pk):
+      obj = Post.objects.get(pk=pk)
+      form = PostForm()
+
+      context = {
+            'obj': obj,
+            'form': form,
+      }
+
+      return render(request, 'posts/detail.html', context)
+
 def load_post_data_view(request, num_posts):
       if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
             visible = 3
@@ -49,6 +60,17 @@ def load_post_data_view(request, num_posts):
                   data.append(item)
             return JsonResponse({'data':data[lower:upper], 'size': size })
 
+def post_detail_data_view(request, pk):
+      obj = Post.objects.get(pk=pk)
+      data = {
+            'id': obj.id,
+            'title': obj.title,
+            'description': obj.description,
+            'author': obj.author.user.username,
+            'logged_in': request.user.username,
+      }
+      return JsonResponse({'data': data})
+
 def like_unlike_post(request):
       if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
             pk = request.POST.get('pk')
@@ -61,5 +83,21 @@ def like_unlike_post(request):
                   obj.liked.add(request.user)
             return JsonResponse({'liked': liked, 'count': obj.like_count})
 
-def hello_world_view(request):
-        return JsonResponse({'text': 'hello world x2'})
+def update_post(request, pk):
+      obj = Post.objects.get(pk=pk)
+      if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+            new_title = request.POST.get('title')
+            new_description = request.POST.get('description')
+            obj.title = new_title
+            obj.description = new_description
+            obj.save()
+            return JsonResponse({
+                  'title': new_title,
+                  'description': new_description,
+            })
+
+def delete_post(request, pk):
+      obj = Post.objects.get(pk=pk)
+      if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+            obj.delete()
+            return JsonResponse({})
