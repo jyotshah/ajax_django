1ecb354f1159046e553ea58131ac0eddc67831a4
Jyot Chirag Shah <Jshah1717@conestogac.on.ca>
2025-03-20 21:33:37 -0400

    configured the like button, added form to the modal and created posts with ajax

--- posts/static/posts/main.js
+++ posts/static/posts/main.js
@@ -5,6 +5,56 @@ const spinnerBox = document.getElementById('spinner-box')
 const loadBtn = document.getElementById('load-btn')
 const endBox = document.getElementById('end-box')
 
+const postForm = document.getElementById('post-form')
+const title = document.getElementById('id_title')
+const description = document.getElementById('id_description')
+const csrf = document.getElementsByName('csrfmiddlewaretoken')
+
+const alertBox = document.getElementById('alert-box')
+console.log('csrf', csrf[0].value)
+
+const getCookie =(name) => {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+const csrftoken = getCookie('csrftoken');
+
+const likeUnlikePosts = ()=> {
+    const likeUnlikeForms = [...document.getElementsByClassName('like-unlike-forms')]
+    likeUnlikeForms.forEach(form=> form.addEventListener('submit', e=>{
+        e.preventDefault()
+        const clickedId = e.target.getAttribute('data-form-id')
+        const clickedBtn = document.getElementById(`like-unlike-${clickedId}`)
+
+        $.ajax({
+            type: 'POST',
+            url: "like-unlike/",
+            data: {
+                'csrfmiddlewaretoken': csrftoken,
+                'pk': clickedId,
+            },
+            success: function(response){
+                console.log(response)
+                clickedBtn.textContent = response.liked ? `Unlike (${response.count})`: `Like (${response.count})`
+            },
+            error: function(error){
+                console.log(error)
+            }
+        })
+    }))
+}
+
 let visible = 3
 
 
@@ -30,14 +80,17 @@ const getData = () => {
                                 <div class="col-1">
                                     <a href="#" class="btn btn-primary">Details</a>
                                 </div>
-                                <div class="col-1">
-                                    <a href="#" class="btn btn-primary">Like</a>
-                                </div>
+                                <div class="col-2">
+                                <form class="like-unlike-forms" data-form-id="${el.id}">
+                                    <button href="#" class="btn btn-primary" id="like-unlike-${el.id}">${el.liked ? `Unlike (${el.count})`: `Like (${el.count})`}</button>
+                                </form>
+                                    </div>
                             </div>
                         </div>
                     </div>
                 `
             });
+            likeUnlikePosts()
             }, 100)
             console.log(response.size)
             if (response.size == 0) {
@@ -58,6 +111,50 @@ loadBtn.addEventListener('click', ()=>{
     spinnerBox.classList.remove('not-visible')
     visible += 3
     getData()
+})
+
+postForm.addEventListener('submit', e=>{
+    e.preventDefault()
+
+    $.ajax({
+        type: 'POST',
+        url: '',
+        data: {
+            'csrfmiddlewaretoken': csrf[0].value,
+            'title': title.value,
+            'description': description.value
+        },
+        success: function(response){
+            console.log(response)
+            postsBox.insertAdjacentHTML('afterbegin',`
+                <div class="card mb-2">
+                        <div class="card-body">
+                            <h5 class="card-title">${response.title}</h5>
+                            <p class="card-text">${response.description}</p>
+                        </div>
+                        <div class="card-footer">
+                            <div class="row">
+                                <div class="col-1">
+                                    <a href="#" class="btn btn-primary">Details</a>
+                                </div>
+                                <div class="col-2">
+                                <form class="like-unlike-forms" data-form-id="${response.id}">
+                                    <button href="#" class="btn btn-primary" id="like-unlike-${response.id}">Like (0)</button>
+                                </form>
+                                    </div>
+                            </div>
+                        </div>
+                    </div>
+                `)
+                likeUnlikePosts()
+                $('#addPostModal').modal('hide')
+                handleAlerts('success', 'New post added!')
+        },
+        error: function(error){
+            console.log(error)
+            handleAlerts('danger', 'Oops! Something went wrong...')
+        }
+    })
 })
 
 getData()
--- posts/views.py
+++ posts/views.py
@@ -1,30 +1,65 @@
 from django.shortcuts import render
 from .models import Post
 from django.http import JsonResponse
+from .forms import PostForm
+from profiles.models import Profile
 # Create your views here.
 
 def post_list_and_create(request):
-    qs = Post.objects.all()
-    return render(request,'posts/main.html', {'qs':qs})
+      form = PostForm(request.POST or None)
+      # qs = Post.objects.all()
+
+      if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+            if form.is_valid():
+                  author = Profile.objects.get(user=request.user)
+                  instance = form.save(commit=False)
+                  instance.author = author
+                  instance.save()
+                  return JsonResponse({
+                        'title': instance.title,
+                        'description': instance.description,
+                        'author': instance.author.user.username,
+                        'id': instance.id,
+                  })
+
+      context = {
+            'form': form,
+      }
+                
+      return render(request,'posts/main.html', context)
 
 def load_post_data_view(request, num_posts):
-      visible = 3
-      upper = num_posts
-      lower = upper - visible 
-      size = Post.objects.all().count() 
+      if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+            visible = 3
+            upper = num_posts
+            lower = upper - visible 
+            size = Post.objects.all().count() 
 
-      qs = Post.objects.all()
-      data = []
-      for obj in qs:
-            item = {
-                  'id': obj.id,
-                  'title': obj.title,
-                  'description': obj.description,
-                  'liked': True if request.user in obj.liked.all() else False,
-                  'author': obj.author.user.username
-            }
-            data.append(item)
-      return JsonResponse({'data':data[lower:upper], 'size': size })
+            qs = Post.objects.all()
+            data = []
+            for obj in qs:
+                  item = {
+                        'id': obj.id,
+                        'title': obj.title,
+                        'description': obj.description,
+                        'liked': True if request.user in obj.liked.all() else False,
+                        'count': obj.like_count,
+                        'author': obj.author.user.username
+                  }
+                  data.append(item)
+            return JsonResponse({'data':data[lower:upper], 'size': size })
+
+def like_unlike_post(request):
+      if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+            pk = request.POST.get('pk')
+            obj = Post.objects.get(pk=pk)
+            if request.user in obj.liked.all():
+                  liked = False
+                  obj.liked.remove(request.user)
+            else:
+                  liked = True
+                  obj.liked.add(request.user)
+            return JsonResponse({'liked': liked, 'count': obj.like_count})
 
 def hello_world_view(request):
         return JsonResponse({'text': 'hello world x2'})
